4ddb1a59a089f1e6e42a74215b339b576a2328a5
https://git.ongarde.net/NoahT <nthom039@uottawa.ca>
2022-07-15 12:21:44 -0400

    Button test bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
       HelloTesting
+      <Button>Test</Button>
     </div>
   );
 }
