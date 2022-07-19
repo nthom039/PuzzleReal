510f4dfbe1b24ab4cf8e1abd81cd0cbf65c14cb7
nthom039 <71086250+nthom039@users.noreply.github.com>
2022-07-19 09:32:42 -0400

    router start

--- src/TopNav.js
+++ src/TopNav.js
@@ -1,5 +1,11 @@
 import { Button, Container, Nav, Navbar, Offcanvas } from 'react-bootstrap'
 import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Switch, Route, Link } from "react-router-dom";
+import OurWork from './OurWork';
+import Home from './Home';
+import Ambitious from './Ambitious';
+import Premade from './Premade';
+
 
 function TopNav() {
 
@@ -9,6 +15,7 @@ function TopNav() {
   const handleShow = () => setShow(true);
 
   return (
+    <Router>
     <div className="TopNav">
       <Navbar bg="primary" variant="dark">
         <Container>
@@ -23,18 +30,41 @@ function TopNav() {
             </Offcanvas.Header>
             <Offcanvas.Body>
               <Nav className="justify-content-end flex-grow-1 pe-3">
-                <Nav.Link href="">Home</Nav.Link>
-                <Nav.Link href="">Our Work</Nav.Link>
-                <Nav.Link href="">Premade</Nav.Link>
-                <Nav.Link href="">Make Your Own</Nav.Link>
-                <Nav.Link href="">Ambitious</Nav.Link>
-                <Nav.Link href="">Cart</Nav.Link>
+                <Nav.Link as={Link} to="/Home">Home</Nav.Link>
+                <Nav.Link as={Link} to="/OurWork">Our Work</Nav.Link>
+                <Nav.Link as={Link} to="/Premade">Premade</Nav.Link>
+                {/*<Nav.Link as={Link} to="/MakeYourOwn">Make Your Own</Nav.Link>*/}
+                <Nav.Link as={Link} to="/Ambitious">Ambitious</Nav.Link>
+                {/*<Nav.Link as={Link} to="/Cart">Cart</Nav.Link>*/}
               </Nav>
             </Offcanvas.Body>
           </Offcanvas>
         </Container>
       </Navbar>
     </div>
+    <div>
+      <Switch>
+        <Route path="Home">
+          <Home />
+        </Route>
+        <Route path="OurWork">
+          <OurWork />
+        </Route>
+        <Route path="Premade">
+          <Premade />
+        </Route>
+        {/*<Route path="MakeYourOwn">
+          <MakeYourOwn />
+        </Route>*/}
+        <Route path="Ambitious">
+          <Ambitious />
+        </Route>
+        {/*<Route path="Cart">
+          <Cart />
+        </Route>*/}
+      </Switch>
+    </div>
+    </Router>
   );
 }
 
